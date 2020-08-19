267fdbac1fc57372d1575f230d96f253589f1363
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-19 10:23:00 +0530

    Employee Wage Using Oops

---
+++ InEmployeeWageOops.java
@@ -0,0 +1,8 @@
+package check;
+
+
+//Interface
+interface InEmployeeWageOops{
+	public void addCompanyEmpWage(String company,int empRatePerHr,int numberOfWorkingDay, int maxHrPerMonth );
+	public void computeEmpWage();
+}
